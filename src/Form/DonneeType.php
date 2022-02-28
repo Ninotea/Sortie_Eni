4e56691356129cf788fefc96ef7147f463386819
ninotea <bolot.antoine@gmail.com>
2022-02-28 09:45:10 +0100

    Filtre terminé + modification affichage détail sortie TODO : -> ajout lieu en AJAX, -> ajout groupe privée -> ajout photo de profil

--- src/Form/DonneeType.php
+++ src/Form/DonneeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Donnee\DonneeSorties;
 use App\Entity\Campus;
+use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -17,40 +18,73 @@ class DonneeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $DateDuJour = new DateTime();
+        $DateDuJour->setTime('12','0');
+        $FDateDuJour = date_format($DateDuJour,'Y-m-d'.'\T'.'H:00');
+
         $builder
             ->add('mot', TextType::class,[
                 'label'=>false,
-                'required'=>false,
+                'required' => false,
                 'attr'=>[
                     'placeholder'=>'Rechercher'
                 ]])
             ->add('campus', EntityType::class,[
+                'required' => false, // desactiver si besoin de filtrer obligatoriement les campus
                 'label'=>'Campus : ',
-                'required' => true,
                 'class'=>Campus::class
             ])
             ->add('dateMin', DateTimeType::class,[
                 'html5' => true,
                 'widget' => 'single_text',
+                'required' => false,
+                'attr' => [
+                    'id' => 'inputDateMin',
+                    'value'=>$FDateDuJour
+                    ],
             ])
             ->add('dateMax', DateTimeType::class,[
                 'html5' => true,
                 'widget' => 'single_text',
+                'required' => false,
+                'attr' => ['id' => 'inputDateMax',
+                    'value'=>$FDateDuJour
+                ],
             ])
             ->add('organisateurTrue', CheckboxType::class,[
-                'label' => "dont je suis l'organisateur"
+                'required' => false,
+                'label' => "dont je suis l'organisateur",
+                'attr'=>['class'=>'checkbox']
             ])
             ->add('organisateurFalse', CheckboxType::class,[
-                'label' => "dont je ne suis pas l'organisateur"
+                'required' => false,
+                'label' => "dont je ne suis pas l'organisateur",
+                'attr'=>['class'=>'checkbox']
             ])
             ->add('inscritTrue', CheckboxType::class,[
-                'label' => "auxquelles je suis inscrit"
+                'required' => false,
+                'label' => "auxquelles je suis inscrit",
+                'attr'=>['class'=>'checkbox']
             ])
             ->add('inscritFalse', CheckboxType::class,[
-                'label' => "auxquelles je ne suis pas inscrit"
+                'required' => false,
+                'label' => "auxquelles je ne suis pas inscrit",
+                'attr'=>['class'=>'checkbox']
             ])
             ->add('passee', CheckboxType::class,[
-                'label' => "passées"
+                'required' => false,
+                'label' => "passées",
+                'attr'=>['class'=>'checkbox']
+            ])
+            ->add('ouverte', CheckboxType::class,[
+                'required' => false,
+                'label' => "ouverte",
+                'attr'=>['class'=>'checkbox']
+            ])
+            ->add('creee', CheckboxType::class,[
+                'required' => false,
+                'label' => "Créée",
+                'attr'=>['class'=>'checkbox']
             ])
             ->add('rechercher',SubmitType::class,[
                 'attr'=>['class'=>'soumission'],
@@ -63,7 +97,7 @@ class DonneeType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => DonneeSorties::class, // quel class représente les données
-            'method' => 'GET', // on veut que la requette passe dans l'URL
+            'method' => 'POST',
             //'csrf_protection'=>false //désactivation de la protectionn
         ]);
     }
--- src/Repository/SortieRepository.php
+++ src/Repository/SortieRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Donnee\DonneeDeRecherche;
 use App\Donnee\DonneeSorties;
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -21,30 +22,99 @@ class SortieRepository extends ServiceEntityRepository
         parent::__construct($registry, Sortie::class);
     }
 
-    public function rechercheSortie(DonneeSorties $donnees)
+    public function rechercheSortie(DonneeSorties $donnees,Participant $currentUser)
     {
         // possibilité d'ajouter un paginator ICI
-        return $this->getRequete($donnees)->getQuery()->getResult();
+        //dd($this->getRequete($donnees,$currentUser)->getQuery()->getResult());
+        return $this->getRequete($donnees,$currentUser)->getQuery()->getResult();
     }
 
-    public function getRequete(DonneeSorties $donnees)
+    public function getRequete(DonneeSorties $donnees,Participant $currentUser)
     {
         $query = $this
             ->createQueryBuilder('sorty') //sorty pour sortie recherché (n'est pas une entité sortie)
-            ->select('sorty','etat','li','campus','orga','partici'//
-            )
+            ->select('sorty','etat','li','campus','orga','partici')
             ->join('sorty.unEtat','etat')
             ->join('sorty.lieu','li')
             ->join('sorty.leCampus','campus')
             ->join('sorty.organisateur','orga')
-            ->leftJoin('sorty.participants','partici')
-            /*
-            if(!empty($donnes->mot))
+            ->leftJoin('sorty.participants','partici');
+            if(!empty($donnees->mot))
             {
-                $query->andWhere(); // Titre contient $mot )
+                $query = $query
+                    ->andWhere('sorty.nom LIKE :motRecherche')
+                    ->orWhere('sorty.infosSortie LIKE :motRecherche')
+                    ->setParameter('motRecherche',"%{$donnees->mot}%");
             }
-            */
-        ;
+            if(!empty($donnees->campus))
+            {
+                $query = $query
+                    ->andWhere('campus.id = :campusId') // les nom utilsiée doivent correpsodnre au alias
+                    ->setParameter('campusId', $donnees->campus->getId());
+            }
+            if($donnees->passee === false)
+            {
+                $query = $query
+                    ->andWhere('etat.libelle NOT LIKE :etatPassee')
+                    ->setParameter('etatPassee', 'passée' );
+            }
+            if($donnees->creee === false)
+            {
+                $query = $query
+                    ->andWhere('etat.libelle NOT LIKE :etatCreee')
+                    ->setParameter('etatCreee', 'créée' );
+            }
+            if($donnees->ouverte === false)
+            {
+                $query = $query
+                    ->andWhere('etat.libelle NOT LIKE :etatOuverte')
+                    ->setParameter('etatOuverte', 'ouverte' );
+            }
+
+            /*
+             * FILTRE ORGANISATEUR
+             */
+            // où je ne suis pas organisateur
+            if($donnees->organisateurTrue === false && $donnees->organisateurFalse === true){
+                $query = $query
+                    ->andWhere('orga.id != :currentUserId')
+                    ->setParameter('currentUserId', $currentUser->getId() );
+            //où je suis organisateur
+            }elseif($donnees->organisateurFalse === false && $donnees->organisateurTrue === true){
+                $query = $query
+                    ->andWhere('orga.id = :currentUserId')
+                    ->setParameter('currentUserId', $currentUser->getId() );
+            // où il n'y a pas d'organisteur (ni moi ni les autres)
+            }elseif($donnees->organisateurFalse === false && $donnees->organisateurTrue === false){
+                // Dans le else La requette renverra à chaque fois aucune donnée
+                $query = $query
+                    ->andWhere('orga.id = :currentUserId')
+                    ->andWhere('orga.id != :currentUserId')
+                    ->setParameter('currentUserId', $currentUser->getId() );
+            }// si les deux sont cochés on ne filtre pas
+
+            /*
+             * FILTRE Participant
+             */
+            //où je ne suis pas inscrit
+            if($donnees->inscritTrue === false && $donnees->inscritFalse === true){
+                $query = $query
+                    ->andWhere(' partici.id NOT IN (:currentUser)')
+                    ->orWhere('partici IS NULL') // permet de faire fonctionner la requette, si la table de relation est vide  SQL ne renvoie rien
+                    ->setParameter('currentUser', $currentUser->getId() );
+            //où je suis inscrit
+            }elseif($donnees->inscritFalse === false && $donnees->inscritTrue === true){
+                $query = $query
+                    ->andWhere('partici IN (:currentUser)')
+                    ->setParameter('currentUser', $currentUser );
+            //où je suis ni inscrit ni pas inscrit (donc aucune)
+            }elseif($donnees->inscritFalse === false && $donnees->inscritTrue === false){
+                // Dans le else La requette renverra à chaque fois aucune donnée
+                $query = $query
+                    ->andWhere('partici NOT IN (:currentUser)')
+                    ->andWhere('partici IN (:currentUser)')
+                    ->setParameter('currentUser', $currentUser );
+            }// où Je suis inscrit ou pas inscrit (tout) donc on filtre pas
 
         return $query;
     }
